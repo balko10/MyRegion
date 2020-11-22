088599889a2b505c20a88b70f9aca5b010a1b024
Alexander Crepaz <alexander.crepaz@student.uibk.ac.at>
2020-11-22 22:07:35 +0100

    init

--- app/src/main/java/com/myregion/ListViewActivity.java
+++ app/src/main/java/com/myregion/ListViewActivity.java
@@ -1,14 +1,14 @@
 package com.myregion;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListViewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.list_view);
     }
 }
---
+++ app/src/main/java/com/myregion/SearchActivity.java
@@ -0,0 +1,27 @@
+package com.myregion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search);
+    }
+
+
+
+    final View.OnClickListener loginHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+
+        }
+    };
+
+}
